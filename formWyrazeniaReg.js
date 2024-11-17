ef6159c72138ceee5b4635924e0d2a27ded39c6e
kamilPiHub <pizon792@gmail.com>
2024-11-17 14:37:20 +0100

    dodanie do html

---
+++ formWyrazeniaReg.js
@@ -0,0 +1,178 @@
+function sprawdzaniePoprawnosci() {
+  
+    poprawne = true; 
+
+    if (!sprawdzImie()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzNazwisko()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzUlica()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzNumerDomu()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzNumerMieszkania()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzTelefon()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzDataUrodzenia()) {
+        poprawne = false;
+    }
+   
+    if (!sprawdzPlec()) {
+        poprawne = false;
+    }
+
+    if (!sprawdzMiasto()) {
+        poprawne = false;
+    }
+
+    return poprawne;  // - jesli przejdzie przez wszytskie z true to znaczy ze formularz jest git
+}
+
+function sprawdzImie() {
+    obj = document.getElementById('imie');
+    txt = obj.value;
+    Wyrazenie = /^[a-z]+$/g;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = ""; 
+        return true;
+    } else {
+        obj.style.backgroundColor = "red"; 
+        return false;
+    }
+}
+
+function sprawdzNazwisko() {
+    obj = document.getElementById('nazwisko');
+    txt = obj.value;
+    Wyrazenie = /^[a-z]+$/g;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+
+function sprawdzUlica() {
+    obj = document.getElementById('ulica');
+    txt = obj.value;
+    Wyrazenie = /^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ0-9\s]+$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+function sprawdzNumerDomu() {
+    obj = document.getElementById('numer_domu');
+    txt = obj.value;
+    Wyrazenie = /^[0-9A-Za-z]+$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+function sprawdzNumerMieszkania() {
+    obj = document.getElementById('numer_mieszkania');
+    txt = obj.value;
+    Wyrazenie = /^[0-9A-Za-z]*$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+function sprawdzTelefon() {
+    obj = document.getElementById('telefon');
+    txt = obj.value;
+    Wyrazenie = /^\d{9}$|^(\d{3}[-.\s]?\d{3}[-.\s]?\d{3})$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+function sprawdzDataUrodzenia() {
+    obj = document.getElementById('data_urodzenia');
+    txt = obj.value;
+    Wyrazenie = /^\d{4}-\d{2}-\d{2}$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+function sprawdzPlec() {
+    obj = document.getElementById('plec');
+    txt = obj.value;
+    Wyrazenie = /^(mezczyzna|kobieta|inna)$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
+
+function sprawdzMiasto() {
+    obj = document.getElementById('miasto');
+    txt = obj.value;
+    Wyrazenie = /^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ\s]+$/;
+    wynik = Wyrazenie.test(txt);
+    
+    if (wynik) {
+        obj.style.backgroundColor = "";
+        return true;
+    } else {
+        obj.style.backgroundColor = "red";
+        return false;
+    }
+}
